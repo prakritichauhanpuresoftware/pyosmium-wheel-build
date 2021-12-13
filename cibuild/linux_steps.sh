7d4ffe6fa9f99d29b31e4c771094c96a0a81ce1f
prakritichauhanpuresoftware <67620689+prakritichauhanpuresoftware@users.noreply.github.com>
2021-12-13 11:43:41 +0530

    Update linux_steps.sh

--- cibuild/linux_steps.sh
+++ cibuild/linux_steps.sh
@@ -59,6 +59,7 @@ if [ "$(uname -m)" == "aarch64" ]
 then
     wget https://download-ib01.fedoraproject.org/pub/epel/7/aarch64/Packages/g/geos-3.4.2-2.el7.aarch64.rpm
     rpm -i geos-3.4.2-2.el7.aarch64.rpm
+fi
 
 # install bzip2
 build_new_zlib
