416a2406ece0980e835c872b3d774c08872307c0
prakritichauhanpuresoftware <67620689+prakritichauhanpuresoftware@users.noreply.github.com>
2021-12-15 16:15:40 +0530

    Create linux_before_test.sh

---
+++ cibuild/linux_before_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ "$(uname -m)" == "aarch64" ]
+then
+    wget https://download-ib01.fedoraproject.org/pub/epel/7/aarch64/Packages/g/geos-3.4.2-2.el7.aarch64.rpm
+    rpm -i geos-3.4.2-2.el7.aarch64.rpm
+fi
