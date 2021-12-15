8013e6eaec479343c6b8e2386c36ab47bf179b05
prakritichauhanpuresoftware <67620689+prakritichauhanpuresoftware@users.noreply.github.com>
2021-12-15 19:11:30 +0530

    Update linux_before_test.sh

--- cibuild/linux_before_test.sh
+++ cibuild/linux_before_test.sh
@@ -2,7 +2,7 @@
 
 if [ "$(uname -m)" == "aarch64" ]
 then
-    #wget https://download-ib01.fedoraproject.org/pub/epel/7/aarch64/Packages/g/geos-3.4.2-2.el7.aarch64.rpm
-    #rpm -i geos-3.4.2-2.el7.aarch64.rpm
-    pip install geos
+    wget https://download-ib01.fedoraproject.org/pub/epel/7/aarch64/Packages/g/geos-3.4.2-2.el7.aarch64.rpm
+    rpm -i geos-3.4.2-2.el7.aarch64.rpm
+    rm geos-3.4.2-2.el7.aarch64.rpm
 fi
